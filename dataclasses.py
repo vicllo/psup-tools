22414a794d9ccb81f7f8308e37669398ff4e9847
vicllo <vicllo@users.noreply.github.com>
2021-05-19 14:14:29 +0200

    Added the Selectivity, Course and Course kinds dataclasses

--- dataclasses.py
+++ dataclasses.py
@@ -35,3 +35,42 @@ class WaitingListEvent(Event):
 class PropositionEvent(Event):
     def __init__(self, date, course):
         super(PropositionEvent, self).__init__(date, course)
+
+
+class Selectivity:
+    def __init__(self, places_available, previous_last_entry):
+        self.places_available = places_available
+        self.previous_last_entry = previous_last_entry
+
+
+class Course:
+    def __init__(self, name, kind, internship, selectivity):
+        self.name = name
+        self.kind = kind
+        self.internship = internship
+        self.selectivity = selectivity
+
+
+class CourseKind:
+    def __init__(self, is_selective):
+        self.is_selective = is_selective
+
+
+class Prepa(CourseKind):
+    def __init__(self):
+        super(Prepa, self).__init__(True)
+
+
+class Licence(CourseKind):
+    def __init__(self, is_selective):
+        super(Licence, self).__init__(is_selective)
+
+
+class SimpleLicence(Licence):
+    def __init__(self):
+        super(SimpleLicence, self).__init__(False)
+
+
+class DoubleLicence(Licence):
+    def __init__(self):
+        super(DoubleLicence, self).__init__(True)
