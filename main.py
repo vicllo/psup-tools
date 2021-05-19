01029e2c4caa541e0ad0e48b8703938162d7410e
vicllo <vicllo@users.noreply.github.com>
2021-05-19 23:17:41 +0200

    Add the session read

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     file_name = "database.csv"
     session = Session(file_name)
 
+    session.read()
+
     date = datetime(2021, 5, 27, 19, 20, 00)
     selectivity = Selectivity(370, 500)
     course = Course("St Louis", Prepa(), False, selectivity)
