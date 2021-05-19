0a7d40976a222d979c047fa31c4fd263c8c37561
vicllo <vicllo@users.noreply.github.com>
2021-05-19 14:03:20 +0200

    Added the event dataclasses

--- dataclasses.py
+++ dataclasses.py
@@ -1,13 +1,37 @@
 
 
 class Event:
-    def __init__(self):
-        pass
+    def __init__(self, date, course):
+        """
+        The standard event class
+        :param date: datetime
+        :param course: Course
+        """
+        self.date = date
+        self.course = course
 
 
-class UserEvent(Event):
-    pass
+class AcceptEvent(Event):
+    def __init__(self, date, course):
+        super(AcceptEvent, self).__init__(date, course)
 
 
-class ServerEvent(Event):
-    pass
+class UserRefuseEvent(Event):
+    def __init__(self, date, course):
+        super(UserRefuseEvent, self).__init__(date, course)
+
+
+class ServerRefuseEvent(Event):
+    def __init__(self, date, course):
+        super(ServerRefuseEvent, self).__init__(date, course)
+
+
+class WaitingListEvent(Event):
+    def __init__(self, date, course, place):
+        super(WaitingListEvent, self).__init__(date, course)
+        self.place = place
+
+
+class PropositionEvent(Event):
+    def __init__(self, date, course):
+        super(PropositionEvent, self).__init__(date, course)
