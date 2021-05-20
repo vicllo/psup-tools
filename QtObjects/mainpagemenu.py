0b8b937a3f6e2b3592700cf9249ecdf311b05bfd
vicllo <vicllo@users.noreply.github.com>
2021-05-20 15:21:59 +0200

    Show the buttons for every course

--- QtObjects/mainpagemenu.py
+++ QtObjects/mainpagemenu.py
@@ -3,7 +3,7 @@ from PySide6 import QtWidgets, QtGui
 
 class MenuWidget(QtWidgets.QWidget):
 
-    def __init__(self, parent):
+    def __init__(self, session, parent):
         """
         Defines the menu widget of the game
         :param parent:
@@ -13,6 +13,16 @@ class MenuWidget(QtWidgets.QWidget):
         home_button = QtWidgets.QPushButton(text="Home", icon=QtGui.QIcon("house.png"), parent=self)
         home_button.clicked.connect(parent.go_home)
 
+        courses_buttons = []
+        i = 1
+        for course in session.courses:
+            new_course_button = QtWidgets.QPushButton(text=course, parent=self)
+            new_course_button.move(0, 20*i)
+            new_course_button.course_name = course
+            i += 1
+            new_course_button.clicked.connect(parent.display_course)
+            courses_buttons.append(new_course_button)
+
         all_courses_button = QtWidgets.QPushButton(text="Courses", parent=self)
-        all_courses_button.move(0, 20)
+        all_courses_button.move(0, 80)
         all_courses_button.clicked.connect(parent.display_all_courses)
