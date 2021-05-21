05088fadfe578540b8594d6cc9d20fd2ffff54c6
vicllo <vicllo@users.noreply.github.com>
2021-05-21 23:44:15 +0200

    Update the hiding of old pannels

--- QtObjects/mainpage.py
+++ QtObjects/mainpage.py
@@ -2,6 +2,7 @@ from PySide6 import QtWidgets, QtGui
 from QtObjects.waitingplotwidget import waiting_plot
 from QtObjects.mainpagemenu import MenuWidget
 from QtObjects.global_overview_widget import GlobalOverview
+from QtObjects.add_event_widget import AddEvent
 from QtObjects import *
 
 
@@ -20,20 +21,17 @@ class MainPage(QtWidgets.QSplitter):
         self.height = height
         self.parent = parent
         self.setFixedSize(width, height)
-
         self.menu = MenuWidget(session, parent=self)
         self.plot = waiting_plot([session.courses["St Louis MPSI"]], self)
         self.global_overview = GlobalOverview(session, self)
-
-        self.pannels = [self.plot, self.global_overview]
-
-        self.plot.hide()
-
+        self.add_event_pannel = AddEvent(session, self)
+        self.pannels = {"plot": self.plot, "overview": self.global_overview, "add_event": self.add_event_pannel}
+        self.go_home()
 
     def hide_all_pannels(self):
-        for pannel in self.pannels:
+        self.pannels["plot"] = self.plot
+        for pannel in self.pannels.values():
             pannel.hide()
-
 
     def go_home(self):
         self.hide_all_pannels()
@@ -41,17 +39,15 @@ class MainPage(QtWidgets.QSplitter):
 
     def add_event(self):
         self.hide_all_pannels()
-
+        self.add_event_pannel.show()
 
     def display_all_courses(self):
         self.hide_all_pannels()
-
         self.plot = waiting_plot(self.session.courses.values(), self)
         self.plot.show()
 
     def display_course(self):
         self.hide_all_pannels()
-
         source = self.sender()
         self.plot = waiting_plot([self.session.courses[source.course_name]], self)
         self.plot.show()
