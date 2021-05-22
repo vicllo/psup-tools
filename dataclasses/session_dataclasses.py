06a3039015b6a43efbe2cdb9bf8efb13b53488d3
vicllo <vicllo@users.noreply.github.com>
2021-05-22 14:06:49 +0200

    Now reads the courses files

--- dataclasses/session_dataclasses.py
+++ dataclasses/session_dataclasses.py
@@ -3,12 +3,15 @@ from constants import *
 from PySide6.QtCore import QDateTime, Qt
 
 class Session:
-    def __init__(self, file_name, courses=None):
+    def __init__(self, course_file_name, event_file_name, courses=None):
         if courses is None:
             courses = {}
-        self.file_name = file_name
+
+        self.course_file_name = course_file_name
+        self.event_file_name = event_file_name
         self.courses = courses
-        self.lines_read = 0
+        self.course_lines_read = 0
+        self.event_lines_read = 0
         self.possible_events = {"Accepted": AcceptEvent,
                                 "UserRefused": UserRefuseEvent,
                                 "SchoolRefused": SchoolRefuseEvent,
@@ -29,18 +32,30 @@ class Session:
             self.courses[course.name].add_event(event)
 
     def read(self):
-        with open(self.file_name, "r") as reading_file:
+        with open(self.course_file_name, "r") as reading_file:
             read = reading_file.read()
         for raw_event in read.split("\n"):
             if raw_event:
-                self.lines_read += 1
+                self.course_lines_read += 1
                 selectivity = [None, None]
-                date, name, kind, internship, selectivity[0], selectivity[1], event_type = raw_event.split(",")[:7]
-                date = QDateTime.fromString(date, Qt.DateFormat.ISODate)
-                selectivity = Selectivity(int(selectivity[0]), int(selectivity[1]))
+                name, kind, internship, selectivity[0], selectivity[1] = raw_event.split(",")
                 course = Course(name, kind, internship, selectivity)
+                if not name in self.courses.values():
+                    self.add_course(course)
+
+        with open(self.event_file_name, "r") as reading_file:
+            read = reading_file.read()
+        for raw_event in read.split("\n"):
+            if raw_event:
+                self.event_lines_read += 1
+                date, name, event_type, place = raw_event.split(",")
+                date = QDateTime.fromString(date, Qt.DateFormat.ISODate)
+
+                if name in self.courses:
+                    course = self.courses[name]
+                else:
+                    raise ValueError("Course not found")
                 if event_type == "Waiting":
-                    place = raw_event.split(",")[-1]
                     event = all_event_kinds[event_type](date, course, place)
                 else:
                     event = all_event_kinds[event_type](date, course)
@@ -52,10 +67,10 @@ class Session:
             for event in course.events:
                 events.append([event.date, event])
         lines_to_write = [x[1] for x in sorted(events, key=lambda x:x[0])]
-        if self.lines_read <= len(lines_to_write):
-            with open(self.file_name, "w") as writing_file:
+        if self.event_lines_read <= len(lines_to_write):
+            with open(self.event_file_name, "w") as writing_file:
                 for line_to_write in lines_to_write:
                     writing_file.write(str(line_to_write)+"\n")
         else:
-            print(self.lines_read, len(lines_to_write))
+            print(self.event_lines_read, len(lines_to_write))
             raise MemoryError("The writing output smaller then the input. Check the logfiles if it is still complete")
