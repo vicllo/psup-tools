380d569e0c23ba725a29bbd80da611d66f02ee3f
vicllo <vicllo@users.noreply.github.com>
2021-05-22 12:24:27 +0200

    Add possible events dictionnary Make the dates to QtDatetime format Update the writing protection to write if there is line addition

--- dataclasses/session_dataclasses.py
+++ dataclasses/session_dataclasses.py
@@ -1,7 +1,6 @@
 from dataclasses import *
 from constants import *
-from datetime import datetime
-
+from PySide6.QtCore import QDateTime, Qt
 
 class Session:
     def __init__(self, file_name, courses=None):
@@ -10,6 +9,11 @@ class Session:
         self.file_name = file_name
         self.courses = courses
         self.lines_read = 0
+        self.possible_events = {"Accepted": AcceptEvent,
+                                "UserRefused": UserRefuseEvent,
+                                "SchoolRefused": SchoolRefuseEvent,
+                                "Waiting": WaitingListEvent,
+                                "Proposition": PropositionEvent}
 
     def add_course(self, new_course):
         self.courses[new_course.name] = new_course
@@ -29,7 +33,7 @@ class Session:
                 self.lines_read += 1
                 selectivity = [None, None]
                 date, name, kind, internship, selectivity[0], selectivity[1], event_type = raw_event.split(",")[:7]
-                date = datetime.fromisoformat(date)
+                date = QDateTime.fromString(date, Qt.DateFormat.ISODate)
                 selectivity = Selectivity(int(selectivity[0]), int(selectivity[1]))
                 course = Course(name, kind, internship, selectivity)
                 if event_type == "Waiting":
@@ -45,10 +49,10 @@ class Session:
             for event in course.events:
                 events.append([event.date, event])
         lines_to_write = [x[1] for x in sorted(events, key=lambda x:x[0])]
-        if self.lines_read == len(lines_to_write):
+        if self.lines_read <= len(lines_to_write):
             with open(self.file_name, "w") as writing_file:
                 for line_to_write in lines_to_write:
                     writing_file.write(str(line_to_write)+"\n")
         else:
             print(self.lines_read, len(lines_to_write))
-            raise MemoryError("The writing output was not the same lenght as the input. Check the logfiles if it is still complete")
+            raise MemoryError("The writing output smaller then the input. Check the logfiles if it is still complete")
