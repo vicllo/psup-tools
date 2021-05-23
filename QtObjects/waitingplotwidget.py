ee59a3b759046d38489aa20fabfaabca816f80d9
vicllo <vicllo@users.noreply.github.com>
2021-05-23 15:31:44 +0200

    Update the plot to be dark theme Bug : Imaginary points due to range

--- QtObjects/waitingplotwidget.py
+++ QtObjects/waitingplotwidget.py
@@ -10,15 +10,40 @@ def waiting_plot(courses, parent):
         serie = QLineSeries()
         for event in course.events:
             if type(event) == WaitingListEvent:
-                serie.append(event.date.date().day(), int(event.place))
+                serie.append(event.date.toSecsSinceEpoch(), int(event.place))
+            serie.setName(course.name)
         series.append(serie)
 
+    x_axis = QDateTimeAxis()
+    x_axis.setTickCount(10)
+    x_axis.setFormat("dd MMM")
+    x_axis.setTitleText("Date")
+    x_axis.setRange(QDateTime(2021, 5, 20, 0, 0, 0), QDateTime(2021, 7, 1, 0, 0, 0))
+
+    y_axis = QValueAxis()
+    y_axis.setTickCount(10)
+    y_axis.setLabelFormat("%i")
+    y_axis.setTitleText("Waiting position")
+    y_axis.setRange(0, 1000)
+
+
     chart = QChart()
-    chart.legend().hide()
+
+    # STYLE
+    chart.setTheme(QChart.ChartThemeDark)
+
     for serie in series:
+        serie.attachAxis(x_axis)
+        serie.attachAxis(y_axis)
         chart.addSeries(serie)
-    chart.createDefaultAxes()
+
+
+    chart.addAxis(x_axis, Qt.AlignBottom)
+    chart.addAxis(y_axis, Qt.AlignLeft)
+
+
     chart.setTitle("Waiting list plot of "+", ".join([course.name for course in courses]))
     chart_view = QChartView(chart, parent)
+    chart_view.setRenderHint(QPainter.Antialiasing)
 
     return chart_view
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dataclasses import *
 from QtObjects import *
 from pathlib import Path
 from datetime import datetime
+from PySide6.QtGui import *
 
 from PySide6 import QtWidgets
 
@@ -29,6 +30,22 @@ if __name__ == '__main__':
 
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle(QStyleFactory.create("fusion"))
+
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.WindowText, Qt.white)
+    palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ToolTipBase, Qt.black)
+    palette.setColor(QPalette.ToolTipText, Qt.white)
+    palette.setColor(QPalette.Text, Qt.white)
+    palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ButtonText, Qt.white)
+    palette.setColor(QPalette.BrightText, Qt.red)
+    palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.setPalette(palette)
 
     main_window = MainWindow(session, 300, 100, 640, 480)
     main_window.show()
